a7d3d80150cb1916c61c697e5dff298a83db1b38
matteoborzi <s280104@studenti.polito.it>
2021-01-09 12:19:02 +0100

    src: timer reset is not handled by disable_timer any more

--- timer/lib_timer.c
+++ timer/lib_timer.c
@@ -47,15 +47,15 @@ void disable_timer( uint8_t timer_num )
 {
   if ( timer_num == 0 ){
 		count_cycles = 0;
-		LPC_TIM0->TCR = 0x2;
+		LPC_TIM0->TCR = 0;
   } else if (timer_num == 1){
-		LPC_TIM1->TCR = 0x2;
+		LPC_TIM1->TCR = 0;
   } else if (timer_num == 2) {
 		run_cycles = 0;
-		LPC_TIM2->TCR = 0x2;
+		LPC_TIM2->TCR = 0;
 	} else {
 		run_cycles = 0;
-		LPC_TIM3->TCR = 0x0;
+		LPC_TIM3->TCR = 0;
 	}
 	
   return;
@@ -101,60 +101,7 @@ uint32_t init_timer ( uint8_t timer_num, uint32_t TimerInterval )
 {
   if ( timer_num == 0 ) {
 		LPC_TIM0->MR0 = TimerInterval;
-
-//*** <<< Use Configuration Wizard in Context Menu >>> ***
-// <h> timer0 MCR
-//   <e.0> MR0I
-//	 <i> 1 Interrupt on MR0: an interrupt is generated when MR0 matches the value in the TC. 0
-//	 <i> 0 This interrupt is disabled
-//   </e>
-//   <e.1> MR0R
-//	 <i> 1 Reset on MR0: the TC will be reset if MR0 matches it.
-//	 <i> 0 Feature disabled.
-//   </e>
-//   <e.2> MR0S
-//	 <i> 1 Stop on MR0: the TC and PC will be stopped and TCR[0] will be set to 0 if MR0 matches the TC
-//	 <i> 0 Feature disabled.
-//   </e>
-//   <e.3> MR1I
-//	 <i> 1 Interrupt on MR1: an interrupt is generated when MR0 matches the value in the TC. 0
-//	 <i> 0 This interrupt is disabled
-//   </e>
-//   <e.4> MR1R
-//	 <i> 1 Reset on MR1: the TC will be reset if MR0 matches it.
-//	 <i> 0 Feature disabled.
-//   </e>
-//   <e.5> MR1S
-//	 <i> 1 Stop on MR1: the TC and PC will be stopped and TCR[1] will be set to 0 if MR1 matches the TC
-//	 <i> 0 Feature disabled.
-//   </e>
-//   <e.6> MR2I
-//	 <i> 1 Interrupt on MR2: an interrupt is generated when MR2 matches the value in the TC.
-//	 <i> 0 This interrupt is disabled
-//   </e>
-//   <e.7> MR2R
-//	 <i> 1 Reset on MR2: the TC will be reset if MR2 matches it.
-//	 <i> 0 Feature disabled.
-//   </e>
-//   <e.8> MR2S
-//	 <i> 1 Stop on MR2: the TC and PC will be stopped and TCR[2] will be set to 0 if MR2 matches the TC
-//	 <i> 0 Feature disabled.
-//   </e>
-//   <e.9> MR3I
-//	 <i> 1 Interrupt on MR3: an interrupt is generated when MR3 matches the value in the TC.
-//	 <i> 0 This interrupt is disabled
-//   </e>
-//   <e.10> MR3R
-//	 <i> 1 Reset on MR3: the TC will be reset if MR3 matches it.
-//	 <i> 0 Feature disabled.
-//   </e>
-//   <e.11> MR3S
-//	 <i> 1 Stop on MR3: the TC and PC will be stopped and TCR[3] will be set to 0 if MR3 matches the TC
-//	 <i> 0 Feature disabled.
-//   </e>
 		LPC_TIM0->MCR = 3;
-// </h>
-//*** <<< end of configuration section >>>    ***
 
 		NVIC_EnableIRQ(TIMER0_IRQn);
 		NVIC_SetPriority(TIMER0_IRQn, 1);
@@ -165,7 +112,7 @@ uint32_t init_timer ( uint8_t timer_num, uint32_t TimerInterval )
 		LPC_TIM1->MR0 = TimerInterval;
 		LPC_TIM1->MCR = 3;				
 
-		NVIC_DisableIRQ(TIMER1_IRQn);
+		NVIC_EnableIRQ(TIMER1_IRQn);
 		NVIC_SetPriority(TIMER1_IRQn, 0);	
 		return (1);
   }
